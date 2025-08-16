b8f7eee342abdfbfcc38a2e12aca64ed1f356e8f
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2025-08-16 18:35:04 +0300

    refactor

--- lua/llm-requester/tools.lua
+++ lua/llm-requester/tools.lua
@@ -239,28 +239,8 @@ local function handle_streaming_response(_, data)
     end
 end
 
-local function get_first_filename_from_buffers()
-    -- Get the current tab page number
-    local tabnr = vim.api.nvim_get_current_tabpage()
-
-    -- Get the list of buffers in the current tab page
-    local windows = vim.api.nvim_tabpage_list_wins(tabnr)
-
-    -- Iterate through the buffers and find the first one with a valid filename
-    for _, win_id in ipairs(windows) do
-        local bufnr = vim.api.nvim_win_get_buf(win_id)
-        local filename = vim.api.nvim_buf_get_name(bufnr)
-        if filename ~= "" and filename ~= "[No Name]" then
-            return filename
-        end
-    end
-
-    -- Return nil if no buffer with a valid filename is found
-    return nil
-end
-
 local function make_editor_context()
-    opened_file = get_first_filename_from_buffers()
+    opened_file = utils.get_first_filename_from_buffers()
     if opened_file == nil then
         return ""
     end
